313bc9d6727da9b1953ab655cb97b9ae9d88e1d2
cristian.gaidos <cristian.gaidos@bearingpoint.com>
2016-03-29 10:04:05 +0200

    Cristian Gaidos - initial commit

---
+++ GitTestProject/src/GitTestClass.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+/**
+ * @author cristian.gaidos
+ *
+ */
+public class GitTestClass {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Welcome to the BearingPoint Java EE Lab!", "LigaAC Labs", JOptionPane.PLAIN_MESSAGE);
+	}
+
+}
