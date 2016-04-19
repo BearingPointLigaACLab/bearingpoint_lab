7d80d78000b1cd29001e23416cf73688940bfdbb
Mihut <Mihut@Mihut>
2016-04-19 20:08:02 +0300

    [Mihut Andrei] Full Test :))

--- MyOnlineBankingAlpha/src/test/java/com/bearingpoint/onlinebanking/tests/XMLReaderTestCase.java
+++ MyOnlineBankingAlpha/src/test/java/com/bearingpoint/onlinebanking/tests/XMLReaderTestCase.java
@@ -3,6 +3,9 @@ package com.bearingpoint.onlinebanking.tests;
 import static org.junit.Assert.*;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import org.junit.Test;
 
@@ -20,15 +23,53 @@ public class XMLReaderTestCase {
 		Cluster cluster = xmlFactory.read(getClass().getClassLoader().getResource("sample2.xml"));
 		assertTrue(cluster.getId().equals("cl_001"));
 		assertTrue(cluster.getName().equals("myCluster"));
-		
+		assertTrue(cluster.getFormattedString().equals(
+				"<myCluster>[Paun Corian, Gaidos Cristian, Mihut Andrei, Panfilii Ion, Stoian Diana]"));
 		List<LegalEntity> oameni = cluster.getLegalEntities();
+		List<Account> accounts;
+		List<Transaction> transactions;
+		DateFormat df;
+		Date d;
 		try 
 		{
 			@SuppressWarnings("resource")
-			BufferedReader buf = new BufferedReader(new InputStreamReader
+			BufferedReader bufom = new BufferedReader(new InputStreamReader
 								(new FileInputStream("src/main/resources/oameni.txt")));
+			@SuppressWarnings("resource")
+			BufferedReader bufcont = new BufferedReader(new InputStreamReader
+					(new FileInputStream("src/main/resources/accounts.txt")));
+			@SuppressWarnings("resource")
+			BufferedReader buftran = new BufferedReader(new InputStreamReader
+					(new FileInputStream("src/main/resources/transactions.txt")));
+			@SuppressWarnings("resource")
+			BufferedReader bufform = new BufferedReader(new InputStreamReader
+					(new FileInputStream("src/main/resources/formatedString.txt")));
 			for(LegalEntity om : oameni)
-				assertTrue(om.getName().equals(buf.readLine()));
+			{
+				assertTrue(om.getFormattedString().equals(bufform.readLine()));
+				assertTrue(om.getId().equals(bufom.readLine()));
+				assertTrue(om.getName().equals(bufom.readLine()));
+				assertTrue(om.getAddress().equals(bufom.readLine()));
+				accounts = om.getAccounts();
+				transactions = om.getTransactions();
+				for(Account account : accounts)
+				{
+					assertTrue(account.getId().equals(bufcont.readLine()));
+					assertTrue(account.getAmount() == Double.parseDouble(bufcont.readLine()));
+					assertTrue(account.getCurrency().equals(bufcont.readLine()));
+				}
+				for(Transaction transaction : transactions)
+				{
+					assertTrue(transaction.getId().equals(buftran.readLine()));
+					assertTrue(transaction.getSource().equals(buftran.readLine()));
+					assertTrue(transaction.getDestination().equals(buftran.readLine()));
+					df = new SimpleDateFormat("dd/MM/yyyy");
+					d = df.parse(buftran.readLine());
+					assertTrue(transaction.getDate().equals(d));
+					assertTrue(transaction.getAmount() == Double.parseDouble(buftran.readLine()));
+				}
+			}
+			
 		} 
 		catch (FileNotFoundException e) 
 		{
@@ -38,9 +79,9 @@ public class XMLReaderTestCase {
 		{
 			System.out.println(e);
 		}
-		
+		catch(ParseException e)
+		{
+			System.out.println(e);
+		}	
 	}
-
-	
-
 }
